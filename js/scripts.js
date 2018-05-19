f83afc8a0e9203e395684d05ea761082ccff7f76
PsijicSeht <38445096+PsijicSeht@users.noreply.github.com>
2018-05-19 11:47:04 -0400

    begun for loop to create grid column and row elements

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,32 @@
 $(document).ready(function() {
 
-for (i = 0; i < )
+   
+   //for loop to create seven columns and place them in game cotainer, max width is set at 1/7th% of container
+//    each div column is given the class "column1" , "column2", respectively based on order
+    for (i = 0; i < 8; i++) {
 
+        $column = '<div class="column" + i "></div>' ;
+
+        $('.gameContainer').append($column); 
+        $('.div+i').css(max-width: (1/7) * 100 + '%');
+        $('.div+i').css(background-color: white);
+        }
+
+// for loop to create individual row elements inside each column
+
+    for (j = 0; j < 8; j++) {
+
+        var columnNumber = j;
+
+
+        for(k = 0; k < 7; k++) {
+            $row = '<div class="row' + 'k' + '</div>'
+            $('.column' + 'columnNumber').append
+
+        }
+
+
+    }
 
 
 });
