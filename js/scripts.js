c3cfc07d2c329afe888f071c33f659be7b784855
PsijicSeht <38445096+PsijicSeht@users.noreply.github.com>
2018-05-20 09:40:35 -0500

    erased for loop attampt to create grid through Jquery and started creating game grid through css grid and html. Have created outer container and board columns. Next step is rows.

--- js/scripts.js
+++ js/scripts.js
@@ -1,32 +1,12 @@
 $(document).ready(function() {
 
-   
-   //for loop to create seven columns and place them in game cotainer, max width is set at 1/7th% of container
-//    each div column is given the class "column1" , "column2", respectively based on order
-    for (i = 0; i < 8; i++) {
+   //for loop to create 42 div boxes inside game board container.
 
-        $column = '<div class="column" + i "></div>' ;
-
-        $('.gameContainer').append($column); 
-        $('.div+i').css(max-width: (1/7) * 100 + '%');
-        $('.div+i').css(background-color: white);
-        }
-
-// for loop to create individual row elements inside each column
-
-    for (j = 0; j < 8; j++) {
-
-        var columnNumber = j;
+   for (i = 0; i < 7; i++) {
+    
 
 
-        for(k = 0; k < 7; k++) {
-            $row = '<div class="row' + 'k' + '</div>'
-            $('.column' + 'columnNumber').append
-
-        }
 
 
-    }
-
-
+   };
 });
