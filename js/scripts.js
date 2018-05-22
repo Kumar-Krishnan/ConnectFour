52122cc1d0d87940710fcf28129c0eeff19797c8
PsijicSeht <38445096+PsijicSeht@users.noreply.github.com>
2018-05-22 11:39:57 -0400

    created loop to create 42 element 2d array. Starting click functions for visual grid

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,24 @@
 $(document).ready(function() {
 
-   //for loop to create 42 div boxes inside game board container.
+   //for loop to create 42 elements for 2d array.
+   var greaterObjectArray = []
 
+   for (x = 0; x < 7; x++) {
+   
+       var newXArray = []
+   
+       for (y = 0; y < 6; y++ ) {
+   
+       var newYValue = 0
+   
+   
+   
+       newXArray.push(newYValue)
+       }
+   
+      
+       greaterObjectArray.push(newXArray)
+   }
     
-
-
-
-
-   };
+$('clickable')
 });
