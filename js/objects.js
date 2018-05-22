91452219dd6b4abc6b2887546f5140a1072b7cd5
PsijicSeht <38445096+PsijicSeht@users.noreply.github.com>
2018-05-22 12:11:56 -0400

    prototype clickable object to allow user to change array values from 0 to 1 or 2

--- js/objects.js
+++ js/objects.js
@@ -1,72 +1,91 @@
-var gridArray = [[object1,object2,object3,object4,object5,object6],[object1,object2,object3,object4,object5,object6],[object1,object2,object3,object4,object5,object6],[object1,object2,object3,object4,object5,object6],[object1,object2,object3,object4,object5,object6],[object1,object2,object3,object4,object5,object6],[object1,object2,object3,object4,object5,object6]];
+// var gridArray = [[object1,object2,object3,object4,object5,object6],[object1,object2,object3,object4,object5,object6],[object1,object2,object3,object4,object5,object6],[object1,object2,object3,object4,object5,object6],[object1,object2,object3,object4,object5,object6],[object1,object2,object3,object4,object5,object6],[object1,object2,object3,object4,object5,object6]];
 
-var displayArray = get.elementsbyclass('row')
-//maybe place inside objects, so that if a object if flipped, it triggers the graphical representation.
-var viewCreator = {
-
-
-
-
-}
-
-var gridInteractionEventHandler = {
+// var displayArray = get.elementsbyclass('row')
+// //maybe place inside objects, so that if a object if flipped, it triggers the graphical representation.
+// var viewCreator = {
 
 
 
 
-}
+// }
 
-
-Example Object properties {
-    valid: false
-    color:
-    ownerOfTile: noOne, Black, Red
-}
-
- var winChecker = {
+// var gridInteractionEventHandler = {
 
 
 
-    var checkerloop = function(){
-        //maybe three funcitons nested in this one. One horizontal check. One vertical. One diagonal.
-        numberOfRed = []
-        numberOfBlack = []
-        for(i = 0; ) {
+
+// }
+
+
+// Example Object properties {
+//     valid: false
+//     color:
+//     ownerOfTile: noOne, Black, Red
+// }
+
+//  var winChecker = {
 
 
 
-        }
-    }
- }
-
- var objectCreator = {
-
-
- }
-
-
- function gridCreator(x,y) {
-    this.x = x,
-    this.y = y,
-    this.valid = false,
-    this.color = "white"
-}
-
-var greaterObjectArray = []
-
- for (x = 0; x < 7; x++) {
-
-    var newXArray = []
-
-    for (y = 0; y < 6; y++ ) {
-
-    var newYArray = [0,1,2]
+//     var checkerloop = function(){
+//         //maybe three funcitons nested in this one. One horizontal check. One vertical. One diagonal.
+//         numberOfRed = []
+//         numberOfBlack = []
+//         for(i = 0; ) {
 
 
 
-    newXArray.push(newYArray)
-    }
+//         }
+//     }
+//  }
+
+//  var objectCreator = {
+
+
+//  }
+
+
+//  function gridCreator(x,y) {
+//     this.x = x,
+//     this.y = y,
+//     this.valid = false,
+//     this.color = "white"
+// }
+
+// var greaterObjectArray = []
+
+//  for (x = 0; x < 7; x++) {
+
+//     var newXArray = []
+
+//     for (y = 0; y < 6; y++ ) {
+
+//     var newYArray = [0,1,2]
+
+
+
+//     newXArray.push(newYArray)
+//     }
 
    
-    greaterObjectArray.push()
+//     greaterObjectArray.push()
+// }
+
+playCount = 1
+
+var ClickableObject = {
+
+    changeColor : function (x,y){
+        if (playCount%2 === 1) {
+            greaterObjectArray[x][y] = 1
+        } 
+        else {
+            greaterObjectArray[x][y] = 2
+        }
+
+        playCount = playCount +1 
+
+
+
+    }
 }
--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
 
-   //for loop to create 42 elements for 2d array.
+   //for loop to create 42 elements for 2d array all initial values set to 0.
    var greaterObjectArray = []
 
    for (x = 0; x < 7; x++) {
@@ -19,6 +19,29 @@ $(document).ready(function() {
       
        greaterObjectArray.push(newXArray)
    }
-    
-$('clickable')
+
+   console.log(greaterObjectArray)
+// 
+
+// Clickable object
+playCount = 1
+
+var ClickableObject = {
+
+    changeColor : function (x,y){
+        if (playCount%2 === 1) {
+            greaterObjectArray[x][y] = 1
+        } 
+        else {
+            greaterObjectArray[x][y] = 2
+        }
+
+        playCount = playCount +1 
+
+
+
+    }
+}
+$('clickable').click(ClickableObject.changeColor(this.attr(x),this.attr(y)))
+
 });
