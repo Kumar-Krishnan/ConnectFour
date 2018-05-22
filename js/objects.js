88d7c0add42af91cefaa3ff3230489775c7f82f4
PsijicSeht <38445096+PsijicSeht@users.noreply.github.com>
2018-05-21 19:29:43 -0500

    created objects.js to pseudocode and model objects needed

---
+++ js/objects.js
@@ -0,0 +1,40 @@
+var gridArray= [[object1,object2,object3,object4,object5,object6],[object1,object2,object3,object4,object5,object6],[object1,object2,object3,object4,object5,object6],[object1,object2,object3,object4,object5,object6],[object1,object2,object3,object4,object5,object6],[object1,object2,object3,object4,object5,object6],[object1,object2,object3,object4,object5,object6]];
+
+
+//maybe place inside objects, so that if a object if flipped, it triggers the graphical representation.
+var viewCreator = {
+
+
+
+
+}
+
+var gridInteractionEventHandler = {
+
+
+
+
+}
+
+
+Example Object properties {
+    valid: false
+    color:
+    ownerOfTile: noOne, Black, Red
+}
+
+ var winChecker = {
+
+
+
+    var checkerloop = function(){
+        //maybe three funcitons nested in this one. One horizontal check. One vertical. One diagonal.
+        numberOfRed = []
+        numberOfBlack = []
+        for(i = 0; ) {
+
+
+
+        }
+    }
+ }
